adab3613afb28c131d91346f9966de7a9a96536a
pankaj9973447073 <pankaj.goto@gmail.com>
2018-10-10 17:37:41 +0530

    Succesfully SetUp

---
+++ ShoppingBackend/ShoppingBackend/src/main/java/Net/pks/ShoppingBackend/App.java
@@ -0,0 +1,13 @@
+package Net.pks.ShoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
